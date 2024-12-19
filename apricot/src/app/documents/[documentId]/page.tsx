4e031efd1337307e5284c92b50764b1619b51a4b
Liam Morales <2232679@slu.edu.ph>
2024-12-19 20:58:49 +0800

    configured and formatted the tiptap editor.

--- apricot/src/app/documents/[documentId]/page.tsx
+++ apricot/src/app/documents/[documentId]/page.tsx
@@ -9,11 +9,11 @@ const DocumentIdPage = async ({params}: DocumentIdPageProps) => {
     const {documentId} = await params;
 
     return (
-        <div>
-            Document Id : {documentId}
+        <div className="min-h-screen bg-[#FAFBFD]">
+           
             <Editor/>
         </div>
-    )
+    );
      
 }
 
