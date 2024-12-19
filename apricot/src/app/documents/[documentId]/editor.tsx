fa91bcfec229c3265e6a97e39031e662bcc7a58f
Liam Morales <2232679@slu.edu.ph>
2024-12-19 21:37:30 +0800

    Added image resize extension.

--- apricot/src/app/documents/[documentId]/editor.tsx
+++ apricot/src/app/documents/[documentId]/editor.tsx
@@ -9,6 +9,7 @@ import TableCell from '@tiptap/extension-table-cell'
 import TableHeader from '@tiptap/extension-table-header'
 import TableRow from '@tiptap/extension-table-row'
 import Image from '@tiptap/extension-image'
+import ImageResize from 'tiptap-extension-resize-image'
 
 
 export const Editor = () => {
@@ -22,6 +23,7 @@ export const Editor = () => {
         },
         extensions: [StarterKit,
             Image,
+            ImageResize,
             Table, 
             TableCell,
             TableHeader,
